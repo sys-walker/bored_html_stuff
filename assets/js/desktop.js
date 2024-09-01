87d4341d7e11ee890db1c602cf05468dae7da5ac
sys-walker <pere.rollon@gmail.com>
2024-09-01 02:10:58 +0200

    add time widget

--- assets/js/desktop.js
+++ assets/js/desktop.js
@@ -7,3 +7,34 @@ desktop.addEventListener("click", function (e) {
   }
   
 });
+
+document.body.onload = function () {
+    console.log("Desktop loaded");
+
+    let getDate = () => {
+        const d = new Date();
+        const formattedDate = d.toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        }).replace(',', '');
+        return formattedDate;
+    }
+
+    //Load time
+    let timeElement = document.getElementById("timeval")
+    timeElement.innerHTML = getDate();
+
+    setInterval(function() {
+        // Get the current date and time
+        const updatedTime = getDate()
+        timeElement.innerHTML =updatedTime
+        timeElement.style.minWidth = window.getComputedStyle(timeElement).width;
+    }, 1000);
+
+
+
+    
+ 
+    
+}
