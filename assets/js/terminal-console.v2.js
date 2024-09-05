699063d518d92b00db1034fa4255d3af2398653b
sys-walker <pere.rollon@gmail.com>
2024-09-05 12:58:09 +0200

    add comands handling on terminal

--- assets/js/terminal-console.v2.js
+++ assets/js/terminal-console.v2.js
@@ -37,11 +37,16 @@ function _createTerminalWindow(appTitle) {
 }
 
 function _createTerminalConsole() {
-  let console = document.createElement('div');
-  console.className = 'window-console';
+  let consoleContent = document.createElement('div');
+  consoleContent.className = 'window-console';
   let lastloginLine = _createLastLoginLine();
-  console.appendChild(lastloginLine);
-  return console;
+
+  consoleContent.onclick = function () {
+    _writableTerminal(consoleContent);
+  };
+
+  consoleContent.appendChild(lastloginLine);
+  return consoleContent;
 }
 
 function _createLastLoginLine() {
@@ -57,9 +62,7 @@ function _createLastLoginLine() {
         margin:0px;
         font-family:monospace
       "
-    
     >Last login ${_getLastLogin()} on ttys004</p>
-  
   `;
   return lastloginContainer;
 }
@@ -82,3 +85,87 @@ function _getLastLogin() {
   storage_setItem('shell-login-date', newLogin);
   return lastLogin ? lastLogin : newLogin;
 }
+
+function _writableTerminal(consoleContent) {
+  let cmd = ''; //buffer of keyinput
+  let completeLine = '';
+  let originalALstLogin = consoleContent.innerHTML;
+
+  consoleContent.onclick = function () {
+    consoleContent.tabIndex = 0;
+
+    consoleContent.onkeydown = function (e) {
+      if (e.key === 'Enter') {
+        // completeLine,originalALstLogin,this.parentElement,consoleContent
+  
+        switch (completeLine) {
+          case 'clear':
+            consoleContent.innerHTML = '';
+            originalALstLogin = '';
+            break;
+          case 'exit':
+            let desktop = document.getElementById('desktop');
+            desktop.removeChild(this.parentElement);
+            break;
+          case '':
+            break;
+          default:
+            printLineTerminal(`esh: command not found: ${completeLine}`, consoleContent, consoleContent.innerHTML);
+            originalALstLogin = consoleContent.innerHTML;
+            break;
+        }
+
+        cmd = '';
+        completeLine = '';
+      } else if (e.key === 'Backspace') {
+        completeLine = '';
+        cmd = cmd.slice(0, -1);
+        completeLine = cmd;
+        printLineTerminal(completeLine, consoleContent, originalALstLogin);
+      } else {
+        cmd += e.key;
+        completeLine = cmd;
+
+        printLineTerminal(completeLine, consoleContent, originalALstLogin);
+      }
+    };
+
+    console.log('clicked');
+    consoleContent.focus();
+  };
+}
+
+function printLineTerminal(text, consoleContent, firstLineTerminal) {
+  let newLine = `
+          <div style="display: flex; flex-flow: wrap;">
+            <p style="
+                color:#4AF626;
+                margin:0px;
+                font-family:monospace
+              ">${text}</p>
+        `;
+  consoleContent.innerHTML = firstLineTerminal + newLine;
+}
+/* 
+<p style="color: #4AF626;margin: 0px;font-family: monospace;"><span style="color:transparent">.................................</span>root@Experiments</p>
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEEEEEEEEEEEEEEEEEEEE:<span style="color:transparent">......</span>-----------------------</p> 
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEEEEEEEEEEEEEEEEEEEE:<span style="color:transparent">......</span>OS: ExperimentOS</p>
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEE:<span style="color:transparent">........................</span>Host: Unknown</p>
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEE:<span style="color:transparent">........................</span>Kernel: Unknown</p>
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEE:<span style="color:transparent">........................</span>Uptime: Unknown</p> 
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEE:<span style="color:transparent">........................</span>Packages: Unknown</p>
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEE:<span style="color:transparent">........................</span>Shell: esh 1.0</p> 
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEEEEEEEEEEEEEEEEEEEE:<span style="color:transparent">......</span>Resolution: Unknown</p>
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEEEEEEEEEEEEEEEEEEEE:<span style="color:transparent">......</span>DE: Pencil</p> 
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEE:<span style="color:transparent">........................</span>WM: Unknown</p> 
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEE:<span style="color:transparent">........................</span>WM Theme: Pencil</p> 
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEE:<span style="color:transparent">........................</span>Terminal: Element Terminal</p> 
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEE:<span style="color:transparent">........................</span>Terminal Font: Unknown</p>
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEE:<span style="color:transparent">........................</span>CPU: Unknown</p>
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEEEEEEEEEEEEEEEEEEEE:<span style="color:transparent">......</span>GPU: Unknown</p> 
+<p style="color: #4AF626;margin: 0px;font-family: monospace;">:EEEEEEEEEEEEEEEEEEEEEEEE:<span style="color:transparent">......</span>Memory: Unknown</p> 
+
+                                                         
+                                                         
+
+*/
